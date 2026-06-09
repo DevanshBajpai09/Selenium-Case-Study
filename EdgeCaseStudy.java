339bf633bbd33c56138b9e3d630d70f6da38abf7
Devansh bajpai <163446624+DevanshBajpai09@users.noreply.github.com>
2026-06-09 22:46:54 +0530

    Add files via upload

---
+++ EdgeCaseStudy.java
@@ -0,0 +1,36 @@
+package com.CaseStudy1;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class EdgeCaseStudy {
+	WebDriver driver;
+	String Url = "https://www.login.hiox.com/register?referrer=easycalculation.com";
+	@BeforeTest
+	public void Setup() {
+		driver=new EdgeDriver();
+		driver.manage().window().maximize();
+		
+		
+	}
+	@Test
+	public void TestMethod() {
+		driver.get(Url);
+		String pageTitle = driver.getTitle();
+		System.out.println("Page Title" +pageTitle);
+		String pageSource = driver.getPageSource();
+		System.out.println("Page Source" +pageSource);
+		int pageSourceLength = pageSource.length();
+		System.out.println("Page Source Length" +pageSourceLength);
+		
+	}
+	@AfterTest
+	public void EndTest() {
+		driver.close();
+	}
+
+}
